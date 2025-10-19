dd70cd362e03978d615e715d3e7a1658edc589e6
Marc-André Montreuil <mamontreuil@hotmail.com>
2025-10-18 22:54:22 -0400

    Improve review
    Improve review

---
+++ Mogawinch.Website.App/Models/Review.cs
@@ -0,0 +1,15 @@
+namespace Mogawinch.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Date { get; set; }
+
+        public string Author { get; set; }
+    }
+}
