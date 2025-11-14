012aa9b690455281f49fac8b3e1eb1d2b6cf0f01
Marc-André Montreuil <mamontreuil@hotmail.com>
2025-11-14 12:02:04 -0500

    try other stuff for head

--- Mogawinch.Website.App/Program.cs
+++ Mogawinch.Website.App/Program.cs
@@ -16,6 +16,7 @@ namespace Mogawinch.Website.App
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
